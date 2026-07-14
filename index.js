16847edefa8d86303a14ade65814d790e429dfef
shinnnnnn754333 <thachcuoncke2024@gmail.com>
2026-07-14 13:42:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const client = new Client({
 });
 
 // Thay 'TOKEN_CUA_MAY' bằng cái token mày lấy ở Developer Portal
-const TOKEN = MTUyNjQ0NDg2MDc1MDQzMDM3OA.GwLIgD.A1veNhEBx_6CM9T26nkX74lkVELW1wfsciU9OI
+const TOKEN = const TOKEN = process.env.TOKEN;
 
 client.on('messageCreate', (message) => {
     // Không cho bot tự trả lời chính nó
